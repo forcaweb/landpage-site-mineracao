3d3202ceca791153973cce47c304f47896957b0d
forcaweb <forcawebsuporte@gmail.com>
2022-11-08 10:45:07 -0300

    at0811202210

--- src/componets/header/header.js
+++ src/componets/header/header.js
@@ -1,28 +1,54 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
-import * as Icons from 'react-icons/bs';
+import * as IconsBs from 'react-icons/bs';
+import * as IconsBi from 'react-icons/bi';
+import * as IconsAi from 'react-icons/ai';
 import './header.css';
+
+function showMenu() {
+  const menuMobile = document.querySelector('.menu-mobile');
+  menuMobile.classList.toggle('mShow');
+}
 
 export default function Header() {
   return (
     <header>
       <nav>
         <li>
-          <Link to="/">
+          <a href="/">
             <h1>
-              <Icons.BsGem /> Potiguar Minerais
+              <IconsBs.BsGem /> Potiguar Minerais
             </h1>
             <p>Mineração em todo o Brasil</p>
-          </Link>
+          </a>
         </li>
       </nav>
 
-      <nav>
+      <nav className="menu">
         <li>
-          <Link to="/">Home</Link>
+          <a href="/">Home</a>
         </li>
         <li>
-          <Link to="/about">Sobre nós</Link>
+          <a href="/about">Sobre nós</a>
+        </li>
+      </nav>
+
+      <nav className="btn-mobile">
+        <li>
+          <button type="button" onClick={showMenu}>
+            <IconsBi.BiMenuAltRight />
+          </button>
+        </li>
+      </nav>
+
+      <nav className="menu-mobile">
+        <span>
+          <IconsAi.AiOutlineClose onClick={showMenu} />
+        </span>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">Sobre nós</a>
         </li>
       </nav>
     </header>
