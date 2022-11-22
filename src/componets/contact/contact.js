92b28feeee1b4d0b2c9083550f2e5277f3a91bc3
forcaweb <forcawebsuporte@gmail.com>
2022-11-22 15:16:21 -0300

    at2211202215

--- src/componets/contact/contact.js
+++ src/componets/contact/contact.js
@@ -1,9 +1,65 @@
 import * as React from 'react';
+import axios from 'axios';
 import * as IconAi from 'react-icons/ai';
 import * as IconFa from 'react-icons/fa';
+import * as IconMd from 'react-icons/md';
 import './contact.css';
 
 export default function Contact() {
+  let respData = [];
+  function sendDataClients(e) {
+    e.preventDefault();
+    const name = document.getElementById('name');
+    const tell = document.getElementById('tell');
+    const email = document.getElementById('email');
+
+    const nameMsg = document.getElementById('nameMsg');
+    const tellMsg = document.getElementById('tellMsg');
+    const emailMsg = document.getElementById('emailMsg');
+    const btnEnv = document.getElementById('btn-env');
+    const btnActv = document.getElementById('btn-actv');
+
+    axios
+      .post('http://localhost:3333', {
+        name: name.value,
+        tell: tell.value,
+        email: email.value,
+      })
+      .then((response) => {
+        respData = response.data;
+
+        if (respData.name_error) {
+          nameMsg.style.display = 'block';
+          nameMsg.innerText = respData.name_error;
+        } else {
+          nameMsg.style.display = 'none';
+        }
+
+        if (respData.tell_error) {
+          tellMsg.style.display = 'block';
+          tellMsg.innerText = respData.tell_error;
+        } else {
+          tellMsg.style.display = 'none';
+        }
+
+        if (respData.email_error) {
+          emailMsg.style.display = 'block';
+          emailMsg.innerText = respData.email_error;
+        } else {
+          emailMsg.style.display = 'none';
+        }
+
+        if (respData.success !== '') {
+          btnActv.style.display = 'none';
+          btnEnv.style.display = 'flex';
+        }
+        console.log(respData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
     <main className="contact-container">
       <section className="header-contact">
@@ -12,7 +68,7 @@ export default function Contact() {
         </h2>
       </section>
       <section className="form-container">
-        <form action="/" method="POST">
+        <form onSubmit={sendDataClients} action="/" method="POST">
           <label htmlFor="name">Nome da Empresa:</label>
           <input
             type="text"
@@ -21,13 +77,17 @@ export default function Contact() {
             placeholder="Digite o nome de sua empresa..."
           />
 
+          <small id="nameMsg">Sem error</small>
+
           <label htmlFor="tell">Telefone:</label>
           <input
-            type="text"
+            type="phone"
             id="tell"
             name="tell"
             placeholder="Digite seu contato de telefone..."
           />
+
+          <small id="tellMsg">Sem error</small>
 
           <label htmlFor="email">Email:</label>
           <input
@@ -36,7 +96,16 @@ export default function Contact() {
             name="email"
             placeholder="Digite seu contato de email profissional..."
           />
-          <button type="submit">Enviar</button>
+
+          <small id="emailMsg">Sem error</small>
+
+          <button type="submit" id="btn-actv">
+            Enviar
+          </button>
+          <button type="submit" id="btn-env" disabled>
+            OK
+            <IconMd.MdDone />
+          </button>
         </form>
         <h2 className="info-contacts">Nossas redes sociais</h2>
         <nav>
